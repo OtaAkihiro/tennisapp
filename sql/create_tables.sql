dc9d8dcb320bbf604c9d61523a6485cc8ee9774b
Aki Ota <otamvh@umich.edu>
2017-12-25 14:32:11 -0500

    pushing again

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,7 +1,8 @@
 drop table if exists entries;
 
 create table users (
-	username varchar(20) primary key,
+	user_id int primary key auto_increment,
+	username varchar(20),
 	firstname varchar(20), 
 	lastname varchar(20),
 	password varchar(256),
