fd57d55cda83eb0c04b1c4bce87d90b5cc5fca67
Aki Ota <otamvh@umich.edu>
2017-12-30 13:36:21 -0500

    Worked on create tables sql code

--- controllers/main.py
+++ controllers/main.py
@@ -2,6 +2,15 @@ from flask import *
 
 main = Blueprint('main', __name__, template_folder='templates')
 
-@main.route('/')
+@main.route('/', methods=['GET','POST'])
 def main_route():
-	return render_template("main.html")
+
+	#This is if they try to navigate to the home page
+	if request.method == 'GET':
+		if 'username' in session:
+			logged = True
+			username = 'username'
+		logged = False
+		username = ''
+
+	return render_template("main.html",logged=logged,user=username)
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,10 +1,73 @@
-drop table if exists entries;
+drop table if exists Users;
+drop table if exists Friends;
+drop table if exists Photos;
+drop table if exists Play;
+drop table if exists Results;
 
-create table users (
+create table Users (
 	user_id int primary key auto_increment,
 	username varchar(20),
 	firstname varchar(20), 
 	lastname varchar(20),
 	password varchar(256),
-	email varchar(40)
-)
+	email varchar(40),
+	rating varchar(3),
+	city varchar(20),
+	state varchar(20)
+);
+
+create table Friends (
+	friend_id int primary key auto_increment,
+	user_id_1 int not null,
+	user_id_2 int not null,
+	foreign key (user_id_1) references Users(user_id),
+	foreign key (user_id_2) references Users(user_id),
+	`date` timestamp default current_timestamp
+);
+
+create table Photos (
+	pic_id int primary key auto_increment,
+	user_id int not null,
+	foreign key (user_id) references Users(user_id),
+	caption varchar(255),
+	profile boolean,
+	format char(3),
+	`date` timestamp default current_timestamp
+);
+
+create table Play (
+	style_id int primary key auto_increment,
+	user_id int not null,
+	foreign key (user_id) references Users(user_id),
+	hand ENUM('righty', 'lefty') not null,
+	#Following are rated from 1-10, 10 being the best
+	forehand int not null,
+	backhand int not null,
+	volley int not null,
+	serve int not null,
+	return_serve int not null
+);
+
+create table Results (
+	match_id int primary key auto_increment,
+
+	user_id_1 int not null,
+	user_id_2 int not null,
+	user_id_3 int,
+	user_id_4 int,
+	foreign key (user_id_1) references Users(user_id),
+	foreign key (user_id_2) references Users(user_id),
+	foreign key (user_id_3) references Users(user_id),
+	foreign key (user_id_4) references Users(user_id),
+	score varchar (20),
+	`date` timestamp default current_timestamp,
+	indoor boolean
+);
+
+/* The table that will store strategies given players strengths and opponnents weaknesses
+create table Strategies (
+
+
+);
+
+*/
